4cb68e845ac2674c851d6d1a97bc901bff4b1a45
Siiaaa08 <salonibaruaa810@gmail.com>
2025-01-30 16:35:35 -0800

    Custom Hash Map

---
+++ Day-2/customhashmap/CustomLinkedList.java
@@ -0,0 +1,16 @@
+package hashmap.customhashmap;
+
+public class CustomLinkedList {
+        int data;
+        int key;
+        CustomLinkedList next;
+
+        CustomLinkedList(int data,int key){
+            this.data=data;
+            this.key=key;
+            this.next=null;
+
+        }
+    }
+
+
